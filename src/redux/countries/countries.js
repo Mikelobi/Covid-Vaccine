74b58f42a7765f8a054259769bc626f5ed80f204
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-23 12:48:09 +0100

    Call and display individual country data

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,19 +1,20 @@
-import getAPIData from './dataAPI';
+import { getCountryAPIData } from '../../utils/dataAPI';
 
 const LOAD_CONTINENT_DATA = 'covidWatch/countries/LOAD_CONTINENT_DATA';
-const LOAD_COUNTRY = 'covidWatch/countries/LOAD_COUNTRY';
+const LOAD_COUNTRY_DATA = 'covidWatch/countries/LOAD_COUNTRY_DATA';
 
-const initialState = [];
+const initialState = {
+  countries: [],
+};
 
 export default function reducer(state = initialState, action) {
-  const { type, payload } = action;
-  switch (type) {
+  switch (action.type) {
     case LOAD_CONTINENT_DATA:
-      return [...state, ...payload.countries];
-    case LOAD_COUNTRY:
+      return { countries: action.payload.countries };
+    case LOAD_COUNTRY_DATA:
       return {
         ...state,
-        current_country: action.payload,
+        country: action.payload,
       };
     default:
       return state;
@@ -27,9 +28,33 @@ export const continentData = (countries) => ({
   },
 });
 
-export const loadContinentData = () => async (dispatch) => {
-  const apiData = await getAPIData();
-  const countries = Object.values(apiData);
+export const loadContinentData = () => (dispatch) => {
+  const API_URL = 'https://covid-api.mmediagroup.fr/v1/vaccines?continent=africa';
 
-  dispatch(continentData(countries));
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then((res) => {
+      const countries = [];
+      const countriesData = Object.values(res);
+      countriesData.forEach((country) => {
+        countries.push({
+          name: country.All.country,
+          total: country.All.administered,
+          full_vacinated: country.All.people_vaccinated,
+          partial_vaccinated: country.All.people_partially_vaccinated,
+        });
+      });
+      dispatch(continentData(countries));
+    });
+};
+
+export const countryVaccineData = (country) => ({
+  type: LOAD_COUNTRY_DATA,
+  payload: country,
+});
+
+export const loadCountryData = (country) => async (dispatch) => {
+  const countryAPIData = await getCountryAPIData(country);
+  const countryData = countryAPIData;
+  dispatch(countryVaccineData(countryData));
 };
