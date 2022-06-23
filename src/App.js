01c77257d218cf56a9fa3cf33cd774ce5ea92ddf
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-23 11:55:45 +0100

    Add browser router and path to country vaccine data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-/* eslint-disable import/extensions */
-// @ts-nocheck
 import React, { useContext } from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
-import Home from './components/Home/Home';
+import Home from './components/Pages/Home';
+import CountryStats from './components/Pages/CountryStats';
 import Header from './components/Header/Header';
 import AuthContext from './redux/auth-context';
+import classes from './App.module.css';
 
 function App() {
   const ctx = useContext(AuthContext);
   return (
     <>
-      <Header />
       <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
+        {!ctx.isLoggedIn && (
+          <div className={classes.mainContent}>
+            <Login />
+          </div>
+        )}
+        {ctx.isLoggedIn && (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/countries/:name" element={<CountryStats />} />
+              <Route path="*" element={<h1 className={classes.alertHeader}>Page not found!</h1>} />
+            </Routes>
+          </>
+        )}
       </main>
     </>
   );
